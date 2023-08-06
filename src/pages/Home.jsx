bf3c6bc7897f0c5477b5aa4cece7cafa19ff9f11
Lilia <liliagarciahiramatsu@gmail.com>
2023-08-06 13:14:10 -0300

    effects - fetching from data.json

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,21 @@
+import { useState, useEffect } from "react"
 import Carousel from "../components/Carousel"
+import axios from "axios"
 
 export default function Home() {
-    let data = [
-        {id: 'america1', city: "Cancun", photo: "/img/america/cancun.jpg"},
-        {id: 'america2', city: "New York", photo: "/img/america/newyork.jpg"},
-        {id: 'america3', city: "Rio de Janeiro", photo: "/img/america/rioDeJaneiro.jpg"},
-        {id: 'america4', city: "Ushuaia", photo: "/img/america/ushuaia.jpg"},
-        {id: 'asia1' , city: "Bangkok", photo: "/img/asia/bangkok.jpg"},
-        {id: 'asia2' , city: "Pekin", photo: "/img/asia/pekin.jpg"},
-        {id: 'asia3' , city: "Singapur", photo: "/img/asia/singapur.jpg"},
-        {id: 'asia4' , city: "Tokyo", photo: "/img/asia/tokio.jpg"},
-        {id: 'europe1' , city: "Ibiza", photo: "/img/europe/ibiza.jpg"},
-        {id: 'europe2' , city: "London", photo: "/img/europe/london.jpg"},
-        {id: 'europe3' , city: "Paris", photo: "/img/europe/paris.jpg"},
-        {id: 'europe4' , city: "Roma", photo: "/img/europe/roma.jpg"},
-        {id: 'oceania1' , city: "Majuro", photo: "/img/oceania/majuro.jpg"},
-        {id: 'oceania2' , city: "Sidney", photo: "/img/oceania/sidney.jpg"},
-        {id: 'oceania3' , city: "Suva", photo: "/img/oceania/suva.jpg"},
-        {id: 'oceania4' , city: "Wellington", photo: "/img/oceania/wellington.jpg"}
-        ]
+    const [data, setData] = useState([])
+    useEffect(
+        () => {
+            axios('/data.json')
+                .then(res => setData(res.data))
+                .catch(err=>console.log(err))
+        },          // callback que No debe retornar nada y NO puede ser asincrona
+        []          // array de dependencias
+                    // cuando esta vacio, EL EFECTO se ejecuta una unica vez cuando se monta el componente
+                    // cuando tiene variables de dependencias, EL EFECTO se ejecuta (cuando se monta y) CADA VEZ que cambia alguna de esas variables
+        // [show]   // es ese ejemplo CADA VEZ que el booleano cambie, se ejecuta la callback de la linea 24
+    )
+    
     return (
         <main className="w-4/5 ml-[219px] mt-[414px] flex justify-between mb-[400px]">
             <div className="w-[742px] h-[304px] flex flex-col gap-10">
