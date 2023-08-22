6dc3548d60d6c5ad0310250d05a6140a7432dcdd
Lilia García Hiramatsu <liliagarciahiramatsu@gmail.com>
2023-08-22 09:11:32 -0300

    cities and detail (#7)

--- src/pages/Cities.jsx
+++ src/pages/Cities.jsx
@@ -2,37 +2,67 @@ import axios from 'axios'
 import { useEffect, useState, useRef } from 'react'
 import apiUrl from '../apiUrl'
 import CardCity from '../components/CardCity'
+import ErrorCard from '../components/ErrorCard'
 
 export default function Cities() {
     const [cities, setCities] = useState([])
-    const text = useRef()
     const [reEffect, setReEffect] = useState(true)
+    const [error, setError] = useState(false)
+    const text = useRef()
     useEffect(
         () => {
             axios(apiUrl+'cities?city='+text.current.value)
-                .then(res => setCities(res.data.response))
-                .catch(err => console.log(err))
+                //.then(res => console.log(res.data.response))
+                .then(res => {
+                    if (res.data.response.length === 0) {
+                        setError(true)
+                    } else {
+                        setError(false)
+                        setCities(res.data.response)
+                    }
+                })
+                .catch(err => {
+                    console.log(err)
+                    setError(true)
+                })
         }, [reEffect]
     )
     function handleFilter() {
         //console.log(text.current.value)
         setReEffect(!reEffect)
     }
+    // COMO HAGO PARA QUE, LUEGO DEL ERROR 404, CUANDO BORRO EL INPUT ME VUELVAN A APARECER LAS CARDS CORRESPONDIENTES
+    // COMO HAGO PARA QUE IGNORE LOS ESPACIOS AL PPIO Y AL FINAL
     return (
-        <div className='w-full flex flex-col items-center'>
-            <input className='w-[100px] h-[40px] mx-[100px] my-[50px] px-[3px] bg-slate-200 border-gray-500' ref={text} type="text" name="text" id="text" onKeyUp={handleFilter} placeholder='Search city'/>
-            
-            <div className="w-1/3 flex flex-wrap justify-center">
-                {cities.map(each => 
-                    <CardCity
-                        key={each._id}
-                        src={each.photo}
-                        alt={each._id}
-                        text={ each.city }
-                        id={each._id}
-                    />
-                )}
+        <div className='w-full flex flex-col items-center mb-[200px]'>
+            <div className="flex items-center border-b-2 border-blue-500 py-1 mb-20 mt-[200px]">
+                <input className='appearence-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none'
+                    ref={text}
+                    type="text"
+                    name="text"
+                    id="text"
+                    onChange={handleFilter}
+                    placeholder='Search city...'
+                />
             </div>
+            { error ? <ErrorCard /> : 
+                <div className='grid grid-cols-1 gap-3 mt-2 mx-[80px]
+                md:grid-cols-2 md:gap-7
+                lg:grid-cols-3 lg:gap-10
+                2xl:grid-cols-4'>
+                    { cities.map(each =>
+                        <CardCity
+                            key={each._id}
+                            src={each.photo}
+                            alt={each._id}
+                            text={each.city}
+                            textCountry={each.country}
+                            id={each._id}
+                        />
+                        )
+                    }
+                </div>
+            }
         </div>
     )
 }
