74f9fae36fbd863dfd278ea3b1e880d1f1f45118
Lilia <liliagarciahiramatsu@gmail.com>
2023-08-30 10:45:12 -0300

    itineraries done

--- src/pages/CityDetail.jsx
+++ src/pages/CityDetail.jsx
@@ -1,9 +1,10 @@
 import { useEffect } from "react"
-import {  useParams } from "react-router-dom"
-import { Link as Anchor } from 'react-router-dom'
+import { useParams } from "react-router-dom"
+import { Link as Anchor } from "react-router-dom"
 import { useSelector, useDispatch } from "react-redux"
 import city_actions from "../store/actions/cities"
 import CardCityDetail from "../components/CardCityDetail"
+
 const { read_city } = city_actions
 
 export default function CityDetail() {
@@ -12,29 +13,29 @@ export default function CityDetail() {
     // Este hook devuelve un objeto donde cada CLAVE es la ruta dinamica definida en el enrutador
     // y donde cada VALOR es el valor de la URL.
     const dispatch = useDispatch()
-
-    useEffect(() => {
-            dispatch(read_city({ id:city_id }))
-    }, [])
-    
-    const city = useSelector(store => store.cities.city)
+    const city = useSelector((store) => store.cities.city)
     //console.log(city)
+    useEffect(() => {
+            dispatch(read_city({ id: city_id }))
+    }, [])
+
     return (
-        <div className="w-3/4 min-h-screen mx-[50px] mt-4 flex flex-col items-end
+        <div className="w-3/4 min-h-screen mx-[50px] mt-4 flex flex-col
         md:w-4/5 md:mx-[100px]
-        min-[1400px]:mx-[150px]
+        min-[1400px]:w-3/5 min-[1400px]:mx-[300px]
         min-[1700px]:mx-[250px]">
-            <Anchor to={'/cities/'} className="mb-10">
-                <button className="bg-black px-2 py-1 mt-10 rounded text-[12px] text-white cursor-pointer hover:bg-[#282472]
+            <Anchor to={'/cities/'} className="mb-10 flex flex-col items-end">
+                <button className="bg-black px-2 py-1 mt-10 rounded text-[12px] text-white cursor-pointer hover:bg-indigo-700
                 md:px-3 md:py-1 md:rounded-lg md:text-[14px]">
                     Back
                 </button>
             </Anchor>
             <CardCityDetail
-                name_city={city.city}
-                photo_city={city.photo}
-                description_city={city.description}
-                id_city={city.city_id}
+                src={city.photo}
+                alt={city_id}
+                text={city.city}
+                id={city_id}
+                description={city.description}
             />
         </div>
     )
